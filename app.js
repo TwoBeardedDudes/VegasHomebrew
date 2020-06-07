2cd7906a3777dbebccff842aba74e7dd1bf7dbc7
DavidGumm <gumm.david@gmail.com>
2020-06-07 12:35:49 -0700

    update scroll to top button

--- app.js
+++ app.js
@@ -19,3 +19,15 @@ window.addEventListener(
   },
   false
 );
+
+let button = document.querySelector("button.return-top");
+
+button.on("scroll", function(e) {
+    
+  if (this.scrollTop > 147) {
+    button.addClass("fix-position");
+  } else {
+    button.removeClass("fix-position");
+  }
+  
+});
